e505d67d0076e498f67ad60d209a5fea1ddc9d6a
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-05-14 02:55:56 +0300

    ref:speperate api related files

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 // Main application entry point, starts the Express server.
+<<<<<<< HEAD
 import 'dotenv/config';
 import "tsconfig-paths/register.js";
 // import { createApp } from '@/createApp';
@@ -8,6 +9,14 @@ import "tsconfig-paths/register.js";
 //
 //
 import { createApp } from '@/createApp';
+=======
+import 'dotenv/config'; // Load environment variables first
+import "tsconfig-paths/register.js"; // Enables path aliases 
+import { createApp } from '@/../createApp';
+import { config } from '@/config';
+import { logger } from '@/utils/logger';
+
+>>>>>>> 3334e03 (ref:seperate api related files)
 const app = createApp();
 const port = config.port;
 
